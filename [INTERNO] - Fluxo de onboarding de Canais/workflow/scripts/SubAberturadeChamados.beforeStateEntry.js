4dbefe4664b63a3acd90844b3035c17a4f45fc39
zandro3210 <meninodexte16@msn.com>
2018-04-03 09:09:06 -0300

    Dia 02/04/2018 intregação Zendesk mexendo no BeforeStateEntry

--- [INTERNO] - Fluxo de onboarding de Canais/workflow/scripts/SubAberturadeChamados.beforeStateEntry.js
+++ [INTERNO] - Fluxo de onboarding de Canais/workflow/scripts/SubAberturadeChamados.beforeStateEntry.js
@@ -1,15 +1,13 @@
-function beforeStateEntry(sequenceId){
-	log.info("@SubAbeturadeChamados  beforeStateEntry sequenceId: "+sequenceId);
-	if(sequenceId == Activity.EMAIL_CORPORATIVO )
-	{
+function beforeStateEntry(sequenceId) {
+	log.info("@SubAbeturadeChamados  beforeStateEntry sequenceId: " + sequenceId);
+	if (sequenceId == Activity.EMAIL_CORPORATIVO) {
 		solicitacaoEmailcoporativo();
-	} 
-	
-	if(sequenceId == Activity.CRIACAO_COD_CRM )
-	{
-		solicitacaoEmailcoporativo();
-	} 
-	
+	}
+
+	if (sequenceId == Activity.CRIACAO_COD_CRM) {
+		solicitacaoCodigoCRM();
+	}
+
 
 
 
@@ -17,154 +15,148 @@ function beforeStateEntry(sequenceId){
 
 }
 
-function solicitacaoEmailcoporativo(){
+function solicitacaoEmailcoporativo() {
 	var subject = retornaParametrizacao("emailCortitulo");
-	log.info("@SubAbeturadeChamados  beforeStateEntry subject: '"+subject + "'");
+	log.info("@SubAbeturadeChamados  beforeStateEntry subject: '" + subject + "'");
 	var template = "abertura_de_chamados";
 	var params = [];
 
 	var mensagem = "";
-	if (hAPI.getCardValue("tipoSolic") == "master" )
-		mensagem +=  retornaParametrizacao("emailCormaster");
+	if (hAPI.getCardValue("tipoSolic") == "master")
+		mensagem += retornaParametrizacao("emailCormaster");
 	else
 		mensagem += retornaParametrizacao("emailCorfranquia");
 
-	mensagem +="<br/><br/><br/><table>";
-		mensagem +="<tr>";
-			mensagem +="<th>Campos<th/>";
-			mensagem +="<th>Valores<th/>";
-		mensagem +="</tr>";
-		mensagem +="<tr>";
-			mensagem +="<td>Razão Social</td>";
-			mensagem +="<td>" +  hAPI.getCardValue("razaoSocial") +"</td>";
-		mensagem +="</tr>";
-		mensagem +="<tr>";
-			mensagem +="<td>CNPJ</td>";
-			mensagem +="<td>" +  hAPI.getCardValue("nrCnpj") +"</td>";
-		mensagem +="</tr>";
-		mensagem +="<tr>";
-			mensagem +="<td>Sugestão de E-mail</td>";
-			mensagem +="<td>" +  hAPI.getCardValue("sugestaoEmail") +"</td>";
-		mensagem +="</tr>";
-	mensagem +="</table>";
-	
-	var url = "<a style='color:red;' href='" + retornaParametrizacao("nmUrl") + "?token=" + hAPI.getCardValue("token") + "&task="+  Activity.JOIN_CHAMADOS + "&thread=1'>Por favor após terminar atividade clique aqui </a><br />";		
-	params.push({name:"TITULO",value: "E-mail Corporativo - Fluxo onBoard"});
-	params.push({name:"MENSAGEM",value: mensagem});
-	params.push({name:"URL",value: url  });
-	
+	mensagem += "<br/><br/><br/><table>";
+	mensagem += "<tr>";
+	mensagem += "<th>Campos<th/>";
+	mensagem += "<th>Valores<th/>";
+	mensagem += "</tr>";
+	mensagem += "<tr>";
+	mensagem += "<td>Razão Social</td>";
+	mensagem += "<td>" + hAPI.getCardValue("razaoSocial") + "</td>";
+	mensagem += "</tr>";
+	mensagem += "<tr>";
+	mensagem += "<td>CNPJ</td>";
+	mensagem += "<td>" + hAPI.getCardValue("nrCnpj") + "</td>";
+	mensagem += "</tr>";
+	mensagem += "<tr>";
+	mensagem += "<td>Sugestão de E-mail</td>";
+	mensagem += "<td>" + hAPI.getCardValue("sugestaoEmail") + "</td>";
+	mensagem += "</tr>";
+	mensagem += "</table>";
+
+	var url = "<a style='color:red;' href='" + retornaParametrizacao("nmUrl") + "?token=" + hAPI.getCardValue("token") + "&task=" + Activity.JOIN_CHAMADOS + "&thread=1'>Por favor após terminar atividade clique aqui </a><br />";
+	params.push({ name: "TITULO", value: "E-mail Corporativo - Fluxo onBoard" });
+	params.push({ name: "MENSAGEM", value: mensagem });
+	params.push({ name: "URL", value: url });
+
 	var receivers = [];
 	receivers.push(retornaParametrizacao("emailCoremail"));
 	log.info("@SubAbeturadeChamados  beforeStateEntry onNotify() ");
-	onNotify(subject, receivers, template, params,  "text/html" );
+	onNotify(subject, receivers, template, params, "text/html");
 }
-function solicitacaoCodigoCRM(){
+function solicitacaoCodigoCRM() {
+
 	var url = "https://apimanager-homolog.totvs.com/api/token";
 	var headers = [];
-	var string = DsGenericRest.callRequest(url,null,null,"POST",);
+
+
+
+	log.info("@SubAbeturadeChamados start solicitacaoCodigoCRM");
+	var string = DsGenericRest.callRequest(url, null, null, "POST", headers);
 
 }
 var DsGenericRest = {
 
-	callRequest: function(url,params, login,method,headers){
-		var auth = this.getBasicAuth(login);
-		var connection = this.createConnection(url, headers,method);
-		return this.readResponse(connection);
-	},
-	/** Cria a conexao com o endereco informado. **/
-	createConnection: function (url, headers,method){
+	callRequest: function (url, params, login, method, headers) {
 		try {
-			log.info("@dsGenericRest/createConnection diz: urlWithParams = "+url);
-			
-			var url = new java.net.URL(urlWithParams);
-			var conn = url.openConnection();
-			
-			conn.setRequestMethod(method);
 
-			if (headers != null){
-				for (i = 0; i < headers.length; i++) { 
-					conn.setRequestProperty(headers[i].name, "Basic "+ headers[i].key);
-				}
-			}
-			
-			return conn;
-		} catch (e) {
-			log.error("@dsGenericRest/createConnection diz: Erro ao criar conexao: "+e);
-		}
-	},
-	/** Captura o retorno enviado pelo rest. **/
-	readResponse: function (connection){
-		try {
-			var responseCode = connection.getResponseCode();
-			var inputStream = (parseInt(responseCode) == 200) ? connection.getInputStream() : connection.getErrorStream();
-			var inputStreamReader = new java.io.InputStreamReader(inputStream);
-			var input = new java.io.BufferedReader(inputStreamReader);
-			var response = new java.lang.StringBuffer();
-			var inputLine;
-			
-			log.info("@dsGenericRest/readResponse diz: responseCode = "+responseCode);
-			
-			while ((inputLine = input.readLine()) != null) {
-				response.append(inputLine);
-			}
-			
-			input.close();
-			
-			if(responseCode != 200){
-				log.error("@dsGenericRest/readResponse diz: Erro ao efetuar a chamada REST");
-				log.error("@dsGenericRest/readResponse diz: "+response.toString());
-			}
+			var urlParameters = new java.lang.String("grant_type=client_credentials");
+		
+			var postData = 	new java.net.URLEncoder.encode(urlParameters,"UTF-8");
+			log.info("@SubAbeturadeChamados postData:'" + postData+"'");
 
-			return response.toString();
+			log.info("@SubAbeturadeChamados url:" + url);
+			var wurl =  new java.net.URL(url);
+		
+			var conn = wurl.openConnection();
+
+
+			
+
+			conn.setDoOutput(true);
+			conn.setRequestMethod( "POST" );
+			conn.setInstanceFollowRedirects(false);
+			conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+			conn.setRequestProperty("charset", "utf-8");
+			conn.setRequestProperty( "Accept", "*/*" );
+			conn.setRequestProperty( "Authorization", "Basic MUZ5ZDNRUUVlVGVXZ1VmdUd4Y2Q3YnI5d1k0YTowS1lSN1hKU3pjWkJ5N1hKTF9BYjlQUUNLQkVh");
+		
+			conn.setUseCaches(false);
+			var writer = new java.io.OutputStreamWriter(conn.getOutputStream());
+			log.info("@SubAbeturadeChamados conn.getOutputStream():'" + conn.getOutputStream()+"'");
+			writer.write(postData);
+			writer.flush();
+
+
+			
+			log.info("@SubAbeturadeChamados conn.getResponseCode():'" + conn.getResponseCode()+"'");
+			var line;
+			log.info("@SubAbeturadeChamados conn.getInputStream():'" + conn.getInputStream()+"'");
+			var reader = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream()));
+			log.info("@SubAbeturadeChamados reader:'" + reader+"'");
+
+			
+
+			while ((line = reader.readLine()) != null) {
+				log.info("@SubAbeturadeChamados line:'" + line+"'");
+			}
+			writer.close();
+			reader.close();
+
+			
+
+
+
+		
 		} catch (e) {
-			log.error("@dsGenericRest/readResponse diz: Erro ao ler resposta");
-			log.error("@dsGenericRest/readResponse diz: "+e);
+			log.error("@SubAbeturadeChamados/readResponse diz: Erro ao ler resposta");
+			log.error("@SubAbeturadeChamados/readResponse diz: " + e);
 		}
-	},
-	getBasicAuth: function(login){
-		var authEnc = null; 
+
+
+		try{
+
+			var url =  new java.net.URL("http://apimanager-homolog.totvs.com/api/zendesk/1.0/groups");
+			var con =  url.openConnection();
+	
+			// optional default is GET
+			con.setRequestMethod("GET");
+	
+			//add request header
+			con.setRequestProperty("Authorization-zendesk", "Basic bGVhbmRyb0Bha3RpZW5vdy5jb20vdG9rZW46RVNNa0VNRFladnRYMEVWSzBUSTVGRXYxYkgyWm5hbnUxZ3hxY29kUw==");
+			con.setRequestProperty("Authorization", "Bearer e3ad16f6-0a64-308a-8118-482039724c47");
+	
+			
+			log.info("@SubAbeturadeChamados GET con.getResponseCode():'" + con.getResponseCode()+"'");
+			var reader = new java.io.BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
+			log.info("@SubAbeturadeChamados GET reader:'" + reader+"'");
+
+			log.info("@SubAbeturadeChamados GET  con.getResponseCode():'" + con.getResponseCode() +"'");
+			
+			while ((line = reader.readLine()) != null) {
+				reader.append(line);
+				log.info("@SubAbeturadeChamados GET  line:'" + line+"'");
+			}
+			writer.close();
+			reader.close();
+			//print result
 		
-		if(login){
-			var authString = new java.lang.String(login.user+":"+ login.password);
-			var bytes = authString.getBytes();
-			authEnc = java.util.Base64.getEncoder().encodeToString(bytes);
+		}catch(e){
+			log.info("@SubAbeturadeChamados GET  error:'" + e+"'");
 		}
 		
-		return authEnc;
 	}
+
 }
-
-var ConstraintUtils = {
-	/**
-	 * Captura o valor das constraints enviadas ao dataset.
-	 * 
-	 * @param constraints: Array de constraints.
-	 * @param constraintNames: Array com o nome das constraints obrigatorias para o dataset.
-	 * 
-	 * @returns HashMap<String,String>
-	 */
-	getConstraintsValue: function(constraints, constraintNames){
-	    var map = this.constraintToMap(constraints);
-	     
-	    for(var i=0; i<constraintNames.length; i++){
-	        var constraintValue = map.get(constraintNames[i]); 
-	        if(constraintValue == null) log.warn("Constraint "+constraintNames[i]+" esta com valor nulo.");
-	    }
-	     
-	    return map;
-	},
-	/** Converte as Contraints em HashMap **/
-	constraintToMap: function(constraints){
-	    if(constraints == null) return null;
-	     
-	    var map = new java.util.HashMap();
-	         
-	    for(var i=0; i<constraints.length; i++){
-	        map.put(constraints[i].fieldName.toLowerCase(), constraints[i].initialValue);
-	    }
-	     
-	    return map;
-	}		
-}
-
-
